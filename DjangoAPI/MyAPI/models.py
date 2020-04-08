f20b7563e7bf4fe55fafafa1059b120f9764042a
marwaahmed198 <58569044+marwaahmed198@users.noreply.github.com>
2020-04-08 05:07:15 -0400

    add django application files and web page

---
+++ DjangoAPI/MyAPI/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+class approvals(models.Model):
+    GENDER_CHOICES = (('Male', 'Male'),('Female', 'Female'))
+    
+    MARRIED_CHOICES = (	('Yes', 'Yes'),('No', 'No'))
+    
+    
+    GRADUATED_CHOICES = (('Graduate', 'Graduated'),('Not_Graduate', 'Not_Graduate'))
+    
+    
+    SELFEMPLOYED_CHOICES = (('Yes', 'Yes'),('No', 'No')) 
+    
+    CLIENT_CHOICES = (('Yes', 'Yes'),('No', 'No')) 
+    
+    
+    PROPERTY_CHOICES = (('Rural', 'Rural'), ('Semiurban', 'Semiurban'),('Urban', 'Urban'))
+		
+	
+    firstName=models.CharField(max_length=15)
+    lastName=models.CharField(max_length=15)
+    clientStatus=models.CharField(max_length=10  ,choices=CLIENT_CHOICES)
+    dependents=models.IntegerField(default=0)
+    applicantIncome=models.IntegerField()
+    coapplicantIncome=models.IntegerField()
+    loanAmt=models.IntegerField(default=0)
+    loanTerm=models.IntegerField()
+    creditHistory=models.IntegerField()
+    gender=models.CharField(max_length=15, choices=GENDER_CHOICES)
+    married=models.CharField(max_length=15, choices=MARRIED_CHOICES)
+    graduateEducation=models.CharField(max_length=15, choices=GRADUATED_CHOICES)
+    selfEmployed=models.CharField(max_length=15, choices=SELFEMPLOYED_CHOICES)
+    propertyArea=models.CharField(max_length=15, choices=PROPERTY_CHOICES)
+
+def __str__(self):
+     return '{}, {}'.format(self.lastname, self.firstname)
---
+++ DjangoAPI/MyAPI/urls.py
@@ -0,0 +1,18 @@
+
+from . import views
+from .views import decision
+from rest_framework import routers
+from django.urls import path, include
+
+router=routers.DefaultRouter()
+router.register('MyAPI', views.ApprovalsView)
+urlpatterns = [
+    # path('form/', views.myform, name='myform'), 
+    # path('status/', views.approvereject), 
+    # path('api/', include(router.urls)),
+    path('', views.cxcontact, name='cxform'),
+  
+  
+    
+  
+]
